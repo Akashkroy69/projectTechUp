4b479af0e2f1118af194ba14bca711b9278e66d0
akashkroy <akashkroy@hotmail.com>
2024-10-29 02:57:52 +0530

    linked list insertion at beginning of the ll is added

---
+++ c and c++/Datastructures/7. LinkedList/LLtest.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Node{
+    int data;
+    struct Node *next;
+};
+
+// operations on linked list
+// 1. insertion : at begining, at end, at middle
+// at beginning
+void insertAtBeg(struct Node  **head, int data){
+    // new node holds the address of newly allocated memory for the Node
+    struct Node *newNode = (struct Node*)malloc(sizeof(struct Node));
+
+    if(newNode == NULL){
+        printf("\nmemory allocation failed\n");
+        return;
+    }
+
+    newNode->data = data;
+    newNode->next = NULL;
+
+    if (*head == NULL)
+    {
+        // 
+        *head = newNode;
+        printf("\nfirst node : mod_d-->  %d, mod_p-->%p, data: %d\n",head,head,(*head)->data);
+        return;
+    }
+    newNode->next = *head;
+    *head = newNode;
+}
+
+void display(struct Node *head){
+    if (head == NULL)
+    {
+        printf("\nThe linked list is empty.\n-------\n");
+        return;
+    }
+    struct Node *temp = head;
+    while (temp != NULL)
+    {
+        printf("\nNode address: %p, data: %d, next node address: %p\n--------\n",temp,temp->data,temp->next);
+        temp = temp->next;
+    } 
+    
+}
+
+int main(){
+    struct Node* head = NULL;
+    display(head);
+    printf("\n*****\n");
+    insertAtBeg(&head,10); 
+    display(head);
+    printf("\n*****\n");
+    insertAtBeg(&head,20); 
+    display(head);
+    printf("\n*****\n");
+      insertAtBeg(&head,30); 
+    display(head);
+    printf("\n*****\n");
+    insertAtBeg(&head,40); 
+    display(head);
+    printf("\n*****\n");
+
+
+
+
+}
